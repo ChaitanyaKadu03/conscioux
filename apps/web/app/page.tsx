afc054ffeaeb67cba9e92c576034d5ae19f5aa33
Chaitanya Kadu <chaitanyakadu03@gmail.com>
2024-11-30 16:38:28 +0530

    Improving v 0.1.0

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,30 +1,6 @@
 import React from 'react'
 
 const Home = () => {
-  const axios = require('axios');
-
-  let response = null;
-  new Promise(async (resolve, reject) => {
-    try {
-      response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/categories', {
-        headers: {
-          'X-CMC_PRO_API_KEY': '',
-        },
-      });
-    } catch (ex) {
-      response = null;
-      // error
-      console.log(ex);
-      reject(ex);
-    }
-    if (response) {
-      // success
-      const json = response.data;
-      console.log(json);
-      resolve(json);
-    }
-  });
-
   return (
     <div>Home</div>
   )
