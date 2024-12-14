03be2b2b9135e19c273cb93f735ea1bc5b95e8d6
Chaitanya Kadu <chaitanyakadu03@gmail.com>
2024-12-14 22:52:43 +0530

    0.1.8-bug

--- apps/web/components/home/features.tsx
+++ apps/web/components/home/features.tsx
@@ -9,7 +9,7 @@ const Features = () => {
                 return (
                     <div key={info.id} className={`flex ${parseInt(info.id) % 2 != 0 ? "flex-row" : "flex-row-reverse"} justify-between items-center`}>
                         <div className={`flex flex-col gap-4 ${parseInt(info.id) % 2 != 0 ? "text-left" : "text-right items-end"}`}>
-                            <h3 className="h4">{info.title}</h3>
+                            <h2 className="h2">{info.title}</h2>
                             <p className="h6 text-neutral-50 w-[80%]">{info.body}</p>
                         </div>
                         <Image src={info.link} alt={info.title} className="h-[420px] w-fit" width={100} height={100} unoptimized={true}/>
